b49dfd6352a2a88151826ce9f32f506316172ac1
Riley Baker <vishnukey@gmail.com>
2018-12-01 05:19:19 -0700

    started work on level generator

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+
+	public List<LevelDescriptor> levels;
+
+	IEnumerator<LevelDescriptor> levelEnumerator;
+
+	// Use this for initialization
+	void Start () {
+		levelEnumerator = levels.GetEnumerator();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	[System.Serializable]
+	public struct LevelDescriptor{
+		uint minDifficulty;
+		uint maxDifficulty;
+		uint length;
+		uint complexity;
+		Lore lore;
+		List<Spawner> spawners; 
+	}
+}
