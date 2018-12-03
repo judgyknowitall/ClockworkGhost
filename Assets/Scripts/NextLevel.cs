957c08b1a3b42c8ecaea82ae726a9e1c5babd416
CaelumSloan <caelum.sloan1@ucalgary.ca>
2018-12-03 08:32:06 -0700

    Win state end state

--- Assets/Scripts/NextLevel.cs
+++ Assets/Scripts/NextLevel.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 [RequireComponent(typeof(BoxCollider2D))]
 public class NextLevel : MonoBehaviour
@@ -8,7 +9,8 @@ public class NextLevel : MonoBehaviour
     void OnTriggerEnter2D(Collider2D col)
     {
         if (col.tag != "Player") return;
-        FindObjectOfType<LevelManager>().NextLevel();
+        var b = FindObjectOfType<LevelManager>().NextLevel();
+        if (!b) { SceneManager.LoadScene("Menu"); return; }
         FindObjectOfType<Player>().transform.position = Vector2.zero;
     }
 }
