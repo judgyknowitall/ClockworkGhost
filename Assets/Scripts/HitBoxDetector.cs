53186e55245f4d6d879efc888a8e737eb00081df
CaelumSloan <caelum.sloan1@ucalgary.ca>
2018-12-03 16:34:01 -0700

    The attacks are real glitchy idunno why

--- Assets/Scripts/HitBoxDetector.cs
+++ Assets/Scripts/HitBoxDetector.cs
@@ -7,9 +7,16 @@ public class HitBoxDetector : MonoBehaviour {
     [Header(";)")]
     public bool playerInMe = false;
 
+    Player player;
+
+    void Start()
+    {
+        player = FindObjectOfType<Player>();
+    }
+
     void OnTriggerStay2D(Collider2D other)
     {
-        if (other.tag == "Player") playerInMe = true;
+        if (other == player.myHitbox) playerInMe = true;
         else playerInMe = false;
     }
 }
