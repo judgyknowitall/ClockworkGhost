ba26fc6060297fb38789c1ac7776138b924ee860
CaelumSloan <caelum.sloan1@ucalgary.ca>
2018-12-02 23:55:48 -0700

    Bat is meta meta

---
+++ Assets/Black.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Black : MonoBehaviour
+{
+    public void Start()
+    {
+        var color = new Color();
+        ColorUtility.TryParseHtmlString("#323138", out color);
+        GetComponent<Camera>().backgroundColor = color;
+    }
+}
