2153308b41b02456b9c8ad9cf3e1f076f48c939d
CaelumSloan <caelum.sloan1@ucalgary.ca>
2018-12-03 09:13:12 -0700

    Who even knows even more

--- Assets/Scripts/Killed.cs
+++ Assets/Scripts/Killed.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Killed : MonoBehaviour
 {
     TextMeshProUGUI pro;
+    float dead = Mathf.Infinity;
 
     void Start ()
     {
@@ -15,8 +17,16 @@ public class Killed : MonoBehaviour
         player.OnDeath += YOUDIED;
     }
 
+    void Update()
+    {
+        if (Time.time > dead)
+            SceneManager.LoadScene("Menu");
+    }
+
     private void YOUDIED()
     {
+
         pro.enabled=true;
+        dead = Time.time + 10f;
     }
 }
